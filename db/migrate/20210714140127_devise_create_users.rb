973cdd21ecd5f0a4abef0c9e04f900cba1a4d423
programmingnuoh <myth0725@gmail.com>
2021-07-18 22:33:22 +0900

    マイグレーション

--- db/migrate/20210714140127_devise_create_users.rb
+++ db/migrate/20210714140127_devise_create_users.rb
@@ -4,7 +4,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :email,              null: false, default: ""
+      t.string :email,              null: false, default: "", unique: true
       t.string :encrypted_password, null: false, default: ""
 
       ## Recoverable
@@ -32,7 +32,12 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-
+      t.string :name, null:false
+      t.string :last_name, null:false
+      t.string :first_name, null:false
+      t.string :last_name_kana, null:false
+      t.string :first_name_kana, null:false
+      t.date :birth_date, null:false
       t.timestamps null: false
     end
 
