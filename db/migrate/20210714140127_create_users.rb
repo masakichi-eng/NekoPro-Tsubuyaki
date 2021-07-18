e52465be6d8667ec40cd2de080dbf8ce55d67cef
programmingnuoh <myth0725@gmail.com>
2021-07-18 22:18:23 +0900

    devise導入

--- db/migrate/20210714140127_create_users.rb
+++
@@ -1,8 +0,0 @@
-class CreateUsers < ActiveRecord::Migration[6.0]
-  def change
-    create_table :users, comment: 'ユーザー関連' do |t|
-      t.string :name, comment: 'ユーザー名'
-      t.timestamps
-    end
-  end
-end
