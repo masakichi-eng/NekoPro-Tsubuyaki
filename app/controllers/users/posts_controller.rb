19ee984a9b1784fe1a90ab334df521de197869dd
Ryota Higashi <ryo19870130@outlook.jp>
2021-07-19 22:44:35 +0900

    rubocop実行・修正
    user.rbのhas_many :postsにdependent destroyを追加

--- app/controllers/users/posts_controller.rb
+++ app/controllers/users/posts_controller.rb
@@ -13,7 +13,9 @@ class Users::PostsController < ApplicationController
   end
 
   private
-    def post_paramas
-      params.require(:post).permit(:description, :photo).merge(user_id: 1)
-    end
+
+  def post_paramas
+    # 投稿機能のみでuser_idは1固定、ユーザー管理が入ったらcurrent_user.idに変更
+    params.require(:post).permit(:description, :photo).merge(user_id: 1)
+  end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :description, presence: true, length: {maximum: 140}
+  validates :description, presence: true, length: { maximum: 140 }
 
   belongs_to :user
   has_one_attached :photo
