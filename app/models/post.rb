523599dc39878d99660f7049a9b8ff5ead5b0b6b
Ryota Higashi <ryo19870130@outlook.jp>
2021-07-19 22:51:33 +0900

    photoバリデーション設定

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,6 @@
 class Post < ApplicationRecord
   validates :description, presence: true, length: { maximum: 140 }
+  validates :photo, presence: true
 
   belongs_to :user
   has_one_attached :photo
