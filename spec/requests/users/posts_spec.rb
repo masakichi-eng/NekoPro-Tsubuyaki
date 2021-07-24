e8a8c259fbbb17d67a5a8e0f9b71ed1acd323343
Ryota Higashi <ryo19870130@outlook.jp>
2021-07-24 23:01:37 +0900

    request test追加

--- spec/requests/users/posts_spec.rb
+++ spec/requests/users/posts_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "Users::Posts", type: :request do
 
   before do
-    @post = FactoryBot.create(:post)
+    User.create(id: 1, name: 'test')
   end
 
   describe 'GET #new' do
@@ -14,23 +14,40 @@ RSpec.describe "Users::Posts", type: :request do
   end
   
   describe 'POST #create' do
-    it 'リクエストが成功すること' do
-      User.create(id: 1, name: 'test')
-      post users_post_path, params: { post: {description: 'test description', photo: Rack::Test::UploadedFile.new("public/images/text_image_300×300.png", "image/png")}}
-      expect(response.status).to eq 302 
-    end
-
-    it '記事が登録されること' do
-      User.create(id: 1, name: 'test')
-      expect{
+    context 'パラメータが正常な場合' do
+      it 'リクエストが成功すること' do
         post users_post_path, params: { post: {description: 'test description', photo: Rack::Test::UploadedFile.new("public/images/text_image_300×300.png", "image/png")}}
-      }.to change(Post, :count).by(1)
-    end
+        expect(response.status).to eq 302 
+      end
 
-    it 'リダイレクトされること' do
-      User.create(id: 1, name: 'test')
-      post users_post_path, params: { post: {description: 'test description', photo: Rack::Test::UploadedFile.new("public/images/text_image_300×300.png", "image/png")}}
-      expect(response).to redirect_to root_path
+      it '記事が登録されること' do
+        expect{
+          post users_post_path, params: { post: {description: 'test description', photo: Rack::Test::UploadedFile.new("public/images/text_image_300×300.png", "image/png")}}
+        }.to change(Post, :count).by(1)
+      end
+
+      it 'リダイレクトされること' do
+        post users_post_path, params: { post: {description: 'test description', photo: Rack::Test::UploadedFile.new("public/images/text_image_300×300.png", "image/png")}}
+        expect(response).to redirect_to root_path
+      end
+    end
+    context 'パラメータが不正な場合' do
+      it 'リクエストが成功すること' do
+        post users_post_path, params: { post: {description: nil, photo: Rack::Test::UploadedFile.new("public/images/text_image_300×300.png", "image/png")}}
+        expect(response.status).to eq 200
+      end
+
+      it 'ユーザーが登録されないこと' do
+        expect{
+        post users_post_path, params: { post: {description: nil, photo: Rack::Test::UploadedFile.new("public/images/text_image_300×300.png", "image/png")}}
+        }.to_not change(Post, :count)
+      end
+
+      it 'エラーが表示されること' do
+        post users_post_path, params: { post: {description: nil, photo: Rack::Test::UploadedFile.new("public/images/text_image_300×300.png", "image/png")}}
+        expect(response.body).to include "Description can&#39;t be blank"
+      end
+
     end
   end
 end
