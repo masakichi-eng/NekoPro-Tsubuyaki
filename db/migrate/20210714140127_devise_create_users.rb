f1e2cca56f4d25cebccd6cc7803ec679c227e9c1
programmingnuoh <myth0725@gmail.com>
2021-08-04 22:59:53 +0900

    userテーブルカラム変更
    name→nicknameに変更

--- db/migrate/20210714140127_devise_create_users.rb
+++ db/migrate/20210714140127_devise_create_users.rb
@@ -32,7 +32,7 @@ class DeviseCreateUsers < ActiveRecord::Migration[6.0]
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
 
-      t.string :name, null:false
+      t.string :nickname, null:false
       t.string :last_name, null:false
       t.string :first_name, null:false
       t.string :last_name_kana, null:false
