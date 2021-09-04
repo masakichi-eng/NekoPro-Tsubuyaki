e9a97d774cb1d42553c30bd5a4448267a9f62830
Ryota Higashi <ryo19870130@outlook.jp>
2021-09-04 17:07:02 +0900

    requestテスト修正

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -10,23 +10,23 @@ RSpec.describe "Likes", type: :request do
   end
   describe "POST #create" do
     it 'リクエストが成功すること' do
-      post users_post_likes_path(user_id: @user.id, post_id: @post.id), xhr: true
+      post users_post_likes_path(@like.post_id), xhr: true
       expect(response).to be_truthy
     end
     it 'いいねが成功してモデルが増えること' do
       expect do
-        post users_post_likes_path(user_id: @user.id, post_id: @post.id), xhr: true
+        post users_post_likes_path(@like.post_id), xhr: true
       end.to change(Like, :count).by(1)
     end
   end
   describe "delete #destroy" do
     it 'リクエストが成功すること' do
-      delete users_post_likes_path(user_id: @user.id, post_id: @post2.id), xhr: true
+      delete users_post_likes_path(@like2.post_id), xhr: true
       expect(response).to be_truthy
     end
     it 'いいね解除が成功してモデルが減ること' do
       expect do
-        delete users_post_likes_path(user_id: @user.id, post_id: @post2.id), xhr: true
+        delete users_post_likes_path(@like2.post_id), xhr: true
       end.to change(Like, :count).by(-1)
     end
   end
