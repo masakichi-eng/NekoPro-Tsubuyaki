50f90e07d2fd14d385ccf82dafc56b87e33635bd
Ryota Higashi <ryo19870130@outlook.jp>
2021-09-04 14:47:41 +0900

    requestテスト

--- spec/requests/likes_spec.rb
+++ spec/requests/likes_spec.rb
@@ -1,7 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe "Likes", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+  before do
+    @user = User.create(id: 1, name: 'test')
+    @post = FactoryBot.create(:post, user_id: @user.id)
+    @post2 = FactoryBot.create(:post, user_id: @user.id)
+    @like = FactoryBot.build(:like, user: @user, post: @post)
+    @like2 = FactoryBot.create(:like, user: @user, post: @post2)
+  end
+  describe "POST #create" do
+    it 'リクエストが成功すること' do
+      post users_post_likes_path(user_id: @user.id, post_id: @post.id), xhr: true
+      expect(response).to be_truthy
+    end
+    it 'いいねが成功してモデルが増えること' do
+      expect do
+        post users_post_likes_path(user_id: @user.id, post_id: @post.id), xhr: true
+      end.to change(Like, :count).by(1)
+    end
+  end
+  describe "delete #destroy" do
+    it 'リクエストが成功すること' do
+      delete users_post_likes_path(user_id: @user.id, post_id: @post2.id), xhr: true
+      expect(response).to be_truthy
+    end
+    it 'いいね解除が成功してモデルが減ること' do
+      expect do
+        delete users_post_likes_path(user_id: @user.id, post_id: @post2.id), xhr: true
+      end.to change(Like, :count).by(-1)
+    end
   end
 end
