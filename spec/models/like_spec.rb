e372be411c97a26798d3353f3d23ff6196141fbf
Ryota Higashi <ryo19870130@outlook.jp>
2021-09-04 16:36:23 +0900

    modelテストをletに修正

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -2,27 +2,25 @@ require 'rails_helper'
 
 RSpec.describe Like, type: :model do
   describe '#create' do
-    before do
-      @user = User.create(name: 'test')
-      @post = FactoryBot.create(:post)
-      @like = FactoryBot.build(:like, user: @user, post: @post)
-    end
+    let(:user) { User.create(name: 'test') }
+    let(:post) { FactoryBot.create(:post) }
+    let(:like) { FactoryBot.build(:like, user: user, post: post) }
 
     context '成功する場合' do
       it '全て揃っている場合' do
-        expect(@like).to be_valid
+        expect(like).to be_valid
       end
     end
     context '失敗する場合' do
       it 'ユーザーidがない場合' do
-        @like.user_id = nil
-        @like.valid?
-        expect(@like.errors.full_messages).to include("User must exist")
+        like.user_id = nil
+        like.valid?
+        expect(like.errors.full_messages).to include("User must exist")
       end
       it 'ポストidがない場合' do
-        @like.post_id = nil
-        @like.valid?
-        expect(@like.errors.full_messages).to include("Post must exist")
+        like.post_id = nil
+        like.valid?
+        expect(like.errors.full_messages).to include("Post must exist")
       end
     end
   end
