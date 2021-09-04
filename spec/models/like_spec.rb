51dd26a09a818cc9351961bc1082ad122d086f8f
Ryota Higashi <ryo19870130@outlook.jp>
2021-09-04 11:18:24 +0900

    model単体テスト

--- spec/models/like_spec.rb
+++ spec/models/like_spec.rb
@@ -1,5 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Like, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @user = User.create(name: 'test')
+      @post = FactoryBot.create(:post)
+      @like = FactoryBot.build(:like, user: @user, post: @post)
+    end
+
+    context '成功する場合' do
+      it '全て揃っている場合' do
+        expect(@like).to be_valid
+      end
+    end
+    context '失敗する場合' do
+      it 'ユーザーidがない場合' do
+        @like.user_id = nil
+        @like.valid?
+        expect(@like.errors.full_messages).to include("User must exist")
+      end
+      it 'ポストidがない場合' do
+        @like.post_id = nil
+        @like.valid?
+        expect(@like.errors.full_messages).to include("Post must exist")
+      end
+    end
+  end
 end
