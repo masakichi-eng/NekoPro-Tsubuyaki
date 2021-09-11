ae8fdc5eb092b30144de27a22765c1293e33b9d3
Ryota Higashi <ryo19870130@outlook.jp>
2021-09-11 15:47:41 +0900

    Comment.new省略

--- app/controllers/general_controller.rb
+++ app/controllers/general_controller.rb
@@ -1,6 +1,5 @@
 class GeneralController < ApplicationController
   def index
     @posts = Post.includes(:user).page(params[:page]).per(2).order(created_at: :desc)
-    @comment = Comment.new
   end
 end
