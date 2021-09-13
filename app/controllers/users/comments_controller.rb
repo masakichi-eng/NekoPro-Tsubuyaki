9704774ab5fad8eaaa1c1946bcdc2c803bafe608
Ryota Higashi <ryo19870130@outlook.jp>
2021-09-14 00:31:32 +0900

    formのmodel指定削除

--- app/controllers/users/comments_controller.rb
+++ app/controllers/users/comments_controller.rb
@@ -2,10 +2,10 @@ class Users::CommentsController < UserController
   before_action :set_item, only: %i(create destroy)
 
   def create
-    Comment.create(user_id: 1, post_id: @post.id, comment: params[:comment][:comment])
+    Comment.create(user_id: 1, post_id: @post.id, comment: params[:comment])
     redirect_back fallback_location: root_path
   end
-  
+
   def destroy
     Comment.find(params[:id]).destroy
     redirect_back fallback_location: root_path
