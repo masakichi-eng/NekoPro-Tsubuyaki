4b2a6f849950c8b462a30400120d314a06c89b18
Ryota Higashi <ryo19870130@outlook.jp>
2021-09-17 10:44:55 +0900

    コメント投稿の不要なpost.id指定削除

--- app/controllers/users/comments_controller.rb
+++ app/controllers/users/comments_controller.rb
@@ -2,7 +2,7 @@ class Users::CommentsController < UserController
   before_action :set_item, only: %i(create destroy)
 
   def create
-    @post.comments.create(user_id: 1, post_id: @post.id, comment: params[:comment])
+    @post.comments.create(user_id: 1, comment: params[:comment])
     redirect_back fallback_location: root_path
   end
 
